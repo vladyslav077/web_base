c9bb44994f4ce1c4901581d16bc6fc725c4f2224
Volodya132 <56843184+Volodya132@users.noreply.github.com>
2023-12-14 19:08:07 +0200

    fix

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, flash
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+vanv/Scripts\activate
+python app.py
+"""
---
+++ test.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3, 4]
+b = 4
+if b >= len(a):
+    print("результат")
+else:
+    print(a[b])
