47fc2a4b1c6970ad88de8a8b47b465254aa4457e
Volodya132 <56843184+Vova148@users.noreply.github.com>
2024-12-15 17:22:04 +0200

    fix

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
+database = [
+    (0, "Iphone 15 Pro", "деякий опис", 10, 41000, "Телефон", "img.png"),
+    (1, "Iphone 14 Pro", "деякий опис2", 15, 40000, "Телефон", "img_1.png"),
+]
 
+a = [43, 12, 32]
+print(a[0])
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", products=database)
 
 @app.route("/page2")
 def page2():
@@ -14,12 +20,12 @@ def page2():
 
 @app.route("/products")
 def products_page():
-    return render_template("products.html")
+    return render_template("products.html", items=database)
 
 @app.route("/product/<item_id>")
 def product_page(item_id):
-
-    return render_template("product.html")
+    print(item_id)
+    return render_template("product.html", item=database[int(item_id)])
 
 if __name__ == '__main__':
     app.run(debug=True)
